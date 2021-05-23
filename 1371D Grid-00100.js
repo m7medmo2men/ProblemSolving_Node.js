28b099367c5dfa562ae7018ea4a0e7167b89545b
Mohamed Momen <57571862+m7medmo2men@users.noreply.github.com>
2021-05-23 12:40:57 +0200

    Update 1371D Grid-00100.js

--- 1371D Grid-00100.js
+++ 1371D Grid-00100.js
@@ -3,9 +3,6 @@ const readline = require('readline').createInterface({
     output: process.stdout,
 });
 
-const fact = (n) => {
-    return (n === 1) ?  1 :  fact(n - 1) * n;
-}
 
 const input = [];
 readline.on('line', line => input.push(line));
